cbb954e8131a76f10f82e96530f8d7fad4a0a310
jahun <99630193+MrNubi@users.noreply.github.com>
2023-05-25 09:39:55 +0900

    74

--- express/src/controllers/users/index.js
+++ express/src/controllers/users/index.js
@@ -24,34 +24,54 @@ class UserController {
     this.router.get('/detail/:id', this.getUserDetail.bind(this));
     this.router.post('/', this.createUser.bind(this));
   }
-  getUsers(res, req) {
-    res.status(200).json({ users: this.users });
+  getUsers(res, req, next) {
+    try {
+      res.status(200).json({ users: this.users });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  getUserDetail(res, req) {
-    const { id } = req.params;
-    const user = users.find((user) => {
-      user.id === Number(id);
-    });
+  getUserDetail(res, req, next) {
+    try {
+      const { id } = req.params;
 
-    res.status(200).json({ user });
+      const user = users.find((user) => {
+        user.id === Number(id);
+      });
+      if (!user) {
+        throw { status: 404, message: 'caanot find `user` value' };
+      }
+
+      res.status(200).json({ user });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  createUser(res, req) {
-    const { name, age } = req.body;
-    console.log('body', req.body, 'name / age', name, age);
+  createUser(res, req, next) {
+    try {
+      const { name, age } = req.body;
+      console.log('body', req.body, 'name / age', name, age);
 
-    this.users.push({
-      id: new Date().getTime(),
-      name,
-      age,
+      this.users.push({
+        id: new Date().getTime(),
+        name,
+        age,
 
-      // id-> unique 값, 따라서 겹칠 일이 거의 없는 시간값 활용
-      // js 문법: 인수=값 이름일 경우 생략가능 (name: name -> name)
-    });
-    res.status(201).json({ users: this.users });
+        // id-> unique 값, 따라서 겹칠 일이 거의 없는 시간값 활용
+        // js 문법: 인수=값 이름일 경우 생략가능 (name: name -> name)
+      });
+      res.status(201).json({ users: this.users });
+    } catch (err) {
+      next(err);
+    }
   }
 }
+
+// 구글에 express error handlling 검색하면 try-catch문도 묶어서 쓰는 예제들 잘 나옴
+// 하지만 어떤 에러처리를 했는지 한 눈에 알아보기 쉬우려면 이렇게 일일히 err처리를 해 주는게 나음
+//취향차 -> 알아서
 
 const userController = new UserController();
 export default userController;
